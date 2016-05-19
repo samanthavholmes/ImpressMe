b7f9dea4bd920ee0affb7a1dc24f3453a9cb6a11
Katherine Broner & Samantha Holmes <pair+katherinebroner.samanthavholmes@devbootcamp.com>
2016-05-19 18:18:39 -0400

    Add comments

---
+++ app/controllers/comments.rb
@@ -0,0 +1,19 @@
+get '/posts/:post_id/comments/new' do
+  @post = Post.find_by(id: params[:post_id])
+  erb :"/comments/new", layout: false, locals: {post: @post}
+end
+
+post '/posts/:post_id/comments/new' do
+  @post = Post.find_by(id: params[:post_id])
+  if logged_in?
+    @comment = Comment.new(params[:comment].merge(user: current_user, post: @post))
+    if @comment.save
+      redirect "/posts/#{@post.id}"
+    else
+      @errors = @comment.errors.full_messages
+      erb :'/comments/new', layout: false, locals: {post: @post}
+    end
+  else
+    erb :'/unauthorized'
+  end
+end
