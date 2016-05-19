70128d8483166245736346c91b6acb338991afe3
Katherine Broner & Samantha Holmes <pair+katherinebroner.samanthavholmes@devbootcamp.com>
2016-05-19 15:42:39 -0400

    Add index page and ajax for like button

---
+++ app/controllers/posts.rb
@@ -0,0 +1,23 @@
+get '/posts' do
+  @posts = Post.all
+  erb :'/posts/index'
+end
+
+get '/posts/new' do
+  erb :'/posts/new'
+end
+
+post '/posts/new' do
+  # if logged_in?
+    @post = Post.new(params[:post].merge(user: current_user))
+    if @post.save
+      redirect '/posts'
+    else
+      @errors = @post.errors.full_messages
+      erb :'/posts/new'
+    end
+  # else
+  #   halt 403
+  #   erb :'/unauthorized'
+  # end
+end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,12 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+ $('.like-button').on("submit", function(e){
+  e.preventDefault();
+  var likes = $(this).find('.like-count')
+  var url = e.target.action
+  $.ajax({
+    url: url
+  }).done(function(likeCount){
+    $(likes).text(likeCount);
+  })
+ })
 });
